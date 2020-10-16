7cf8655e0357dc3f05f2dc1cbd5f05ae1fa47c46
Rem889 <coderremesian@gmail.com>
2020-10-16 19:48:06 +0600

    format

--- phonebook2.py
+++ phonebook2.py
@@ -17,7 +17,18 @@ class Window(Tk):
         self.geometry("300x250")
         self.resizable(width = False, height = False)
         self.phonebook_list = []
-        self.months = {"01":"January", "02":"February", "03":"Maarch", "04":"April", "05":"May", "06":"June", "07":"July", "08":"August", "09":"September", "10":"October", "11":"November", "12":"December"}
+        self.months = {"01":"January",
+                        "02":"February",
+                        "03":"March",
+                        "04":"April",
+                        "05":"May",
+                        "06":"June",
+                        "07":"July",
+                        "08":"August",
+                        "09":"September",
+                        "10":"October",
+                        "11":"November",
+                        "12":"December"}
         
         self.addPrompt = None
         self.editPrompt = None
